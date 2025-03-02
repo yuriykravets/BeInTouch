d4aaa799f80b851335f68cc750d28eefb206d759
Yuriy Kravets <97830497+yuriykravets@users.noreply.github.com>
2025-03-02 12:30:14 +0200

    Provide system ui controller for Application

--- app/src/main/java/com/partitionsoft/beintouch/MainActivity.kt
+++ app/src/main/java/com/partitionsoft/beintouch/MainActivity.kt
@@ -6,18 +6,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.partitionsoft.beintouch.domain.usecases.AppEntryUseCases
 import com.partitionsoft.beintouch.domain.usecases.RetrieveAppEntry
 import com.partitionsoft.beintouch.presentation.navigation.NavGraph
@@ -40,6 +44,17 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             BeInTouchTheme {
+
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemController = rememberSystemUiController()
+
+                SideEffect {
+                    systemController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
                     val startDestination = viewModel.startDestination
                     NavGraph(startDestination = startDestination)
